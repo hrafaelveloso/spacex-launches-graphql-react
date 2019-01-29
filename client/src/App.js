ae4aaf1e3d3fbd7bd2d7b2e9bbf16eca989311c8
hrafaelveloso <rafaelcostaveloso@gmail.com>
2019-01-29 16:39:18 +0000

    Changed container

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
         <Router>
-          <div className="container-fluid">
+          <div className="container">
             <img src={logo} alt="SpaceX" style={{ width: 400, display: 'block', margin: 'auto' }} />
             <Route exact path="/" component={Launches} />
             <Route exact path="/launch/:flight_number" component={Launch} />
