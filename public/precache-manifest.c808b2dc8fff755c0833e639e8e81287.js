7b128d12af1989b8ea07ce14f157d6d624be3748
hrafaelveloso <rafaelcostaveloso@gmail.com>
2019-01-29 16:21:28 +0000

    build folder

---
+++ public/precache-manifest.c808b2dc8fff755c0833e639e8e81287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f751c040a2fdbdda3010475ed128f86b",
+    "url": "/static/media/logo.f751c040.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7d3f89af6a55001eb279",
+    "url": "/static/js/main.7d3f89af.chunk.js"
+  },
+  {
+    "revision": "40b74d0f8eb65c2491eb",
+    "url": "/static/js/1.40b74d0f.chunk.js"
+  },
+  {
+    "revision": "7d3f89af6a55001eb279",
+    "url": "/static/css/main.b6759c34.chunk.css"
+  },
+  {
+    "revision": "200a666a0f80cd3d2600106c0fd8c546",
+    "url": "/index.html"
+  }
+];
